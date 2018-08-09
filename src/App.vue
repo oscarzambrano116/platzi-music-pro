6ef4344d83e4db8809a0c99e0d803b65798ec7f5
Oscar Zambrano <oscarz.developer@gmail.com>
2018-08-08 23:10:05 -0500

    Adding Track component and Loader component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   #app
     pm-header
-    section.section
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
       nav.nav.has-shadow
         .container
           .field.has-addons
@@ -17,33 +18,39 @@
             small {{ searchMessage }}
 
       .container
-        .columns.results
-          .column(v-for="t in tracks")
-            | {{ t.name }} - {{ t.artists[0].name }}
+        .columns.results.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(:track="t")
     pm-footer
 </template>
 
 <script>
 
 import trackService from './services/track'
-import PmHeader from './components/layout/Header.vue'
-import PmFooter from './components/layout/Footer.vue'
+import PmHeader from '@/components/layout/Header.vue'
+import PmFooter from '@/components/layout/Footer.vue'
+import PmTrack from '@/components/Track.vue'
+import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'app',
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      isLoading: false
     }
   },
   methods: {
     search () {
       if (!this.searchQuery) return
 
+      this.isLoading = true
+
       trackService.search(this.searchQuery)
         .then(res => {
           this.tracks = res.tracks.items
+          this.isLoading = false
         })
     }
   },
@@ -54,7 +61,9 @@ export default {
   },
   components: {
     PmHeader,
-    PmFooter
+    PmFooter,
+    PmTrack,
+    PmLoader
   }
 }
 </script>
