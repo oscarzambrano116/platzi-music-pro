5d26aea54609cd97acd2fb7871d7563125628fb6
Oscar Zambrano <oscarz.developer@gmail.com>
2018-08-09 13:57:49 -0500

    Adding vue-router dependency

--- src/App.vue
+++ src/App.vue
@@ -2,95 +2,21 @@
   #app
     pm-header
 
-    pm-notification(v-show="showNotification")
-      p(slot="body") No se encontraron resultados
+    router-view
 
-    pm-loader(v-show="isLoading")
-    section.section(v-show="!isLoading")
-      nav.nav
-        .container
-          .field.has-addons
-            input.input.is-large(
-              type="text",
-              placeholder="Buscar canciones",
-              v-model="searchQuery",
-            )
-            a.button.is-info.is-large(@click="search") Buscar
-            a.button.is-danger.is-large &times;
-        .container
-          p
-            small {{ searchMessage }}
-
-      .container
-        .columns.results.is-multiline
-          .column.is-one-quarter(v-for="t in tracks")
-            pm-track(
-              :class="{ 'is-active': t.id === selectedTrack }"
-              :track="t",
-              @select="setSelectedTrack"
-            )
     pm-footer
 </template>
 
 <script>
 
-import trackService from './services/track'
 import PmHeader from '@/components/layout/Header.vue'
 import PmFooter from '@/components/layout/Footer.vue'
 
-import PmTrack from '@/components/Track.vue'
-
-import PmLoader from '@/components/shared/Loader.vue'
-import PmNotification from '@/components/shared/Notification.vue'
-
 export default {
   name: 'app',
-  data () {
-    return {
-      searchQuery: '',
-      tracks: [],
-      isLoading: false,
-      selectedTrack: '',
-      showNotification: false
-    }
-  },
-  methods: {
-    search () {
-      if (!this.searchQuery) return
-
-      this.isLoading = true
-
-      trackService.search(this.searchQuery)
-        .then(res => {
-          this.showNotification = res.tracks.total === 0
-          this.tracks = res.tracks.items
-          this.isLoading = false
-        })
-    },
-    setSelectedTrack (id) {
-      this.selectedTrack = id
-    }
-  },
-  computed: {
-    searchMessage () {
-      return `Encontrados ${this.tracks.length}`
-    }
-  },
   components: {
     PmHeader,
-    PmFooter,
-    PmTrack,
-    PmLoader,
-    PmNotification
-  },
-  watch: {
-    showNotification () {
-      if (this.showNotification) {
-        setTimeout(() => {
-          this.showNotification = false
-        }, 3000)
-      }
-    }
+    PmFooter
   }
 }
 </script>
