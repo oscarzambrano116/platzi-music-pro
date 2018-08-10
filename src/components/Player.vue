83eba06abad6c43ba1381bbd314f073f13982d50
Oscar Zambrano <oscarz.developer@gmail.com>
2018-08-10 17:22:37 -0500

    Adding filters folder

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -4,7 +4,7 @@
       img(:src="track.album.images[0].url")
     p
       strong {{ track.name }}
-      small [{{ track.duration_ms }}]
+      small [{{ track.duration_ms | ms-to-mm }}]
     p
       audio(controls, :src="track.preview_url")
 </template>
@@ -18,6 +18,8 @@ export default {
   },
   created () {
     this.$bus.$on('set-track', (track) => {
+      console.log('---track---')
+      console.log(track)
       this.track = track
     })
   }
